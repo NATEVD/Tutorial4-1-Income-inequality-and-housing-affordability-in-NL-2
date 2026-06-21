41e2da4226c24232e6e04566dca20ce585496a4d
tomex101 <toms@clocked.lv>
2026-06-21 13:41:12 +0200

    reproducibility

---
+++ run_all.R
@@ -0,0 +1,8 @@
+# run_all.R
+# Run this script to reproduce the entire analysis from scratch
+
+# Step 1 - restore exact package versions
+renv::restore()
+
+# Step 2 - render the full report
+rmarkdown::render("Template_Assignment.Rmd")
