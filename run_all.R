ca0ef1c3957add48ad8137b3e1695abe2546222b
tomex101 <toms@clocked.lv>
2026-06-21 15:34:08 +0200

    Update renv.lock
    Update renv.lock

--- run_all.R
+++ run_all.R
@@ -1,9 +1,8 @@
 # run_all.R
-
 # Run this script to reproduce the entire analysis from scratch
 
 # Step 1 - restore exact package versions
 renv::restore()
 
-# Step 2 - render the full report
-rmarkdown::render("Template_Assignment.Rmd")
+# Step 2 - render the full report to PDF
+rmarkdown::render("Tutorial_4_group_1_Income_inequality_and_housing_affordabilities_in_The_Netherlands.Rmd")
